b054043ef1da867d07e13a7d44e81ce0398e1e88
Intharah <intharah@gmail.com>
2025-02-19 18:22:20 +0100

    Add Tésée and Nono animations

---
+++ Assets/NonoController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+using UnityEngine.UI;
+using Platformer.Mechanics;
+
+public class NonoController : MonoBehaviour
+{
+    public Transform player;
+
+    public bool isFlipped = false;
+
+    public void Update()
+    {
+        LookAtPlayer();
+    }
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+         if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;        
+        }
+    }
+}
